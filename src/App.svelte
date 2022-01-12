4004bfbf3bb831c3039bf14d9df40ef393197208
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-01-12 14:33:56 -0500

    :sparkles: Example

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,19 @@
 <script>
-  export let name;
+  export let title, src, num;
 </script>
 
 <main>
-  <h1>Hello</h1>
-  <p />
+  <h1>{title}</h1>
+  <img {src} alt="Latest xkcd comic" />
+  <p>{num}</p>
+
+  <footer>
+    <p>
+      <a href="https://github.com/UltiRequiem"
+        >Eliaz Bobadilla (a.k.a UltiRequiem)</a
+      >
+    </p>
+  </footer>
 </main>
 
 <style>
@@ -24,5 +33,11 @@
 
   main {
     text-align: center;
+    margin: 10%;
+  }
+
+  footer {
+    margin-top: 100px;
+    bottom: 0;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import App from "./App.svelte";
 
+// cors problems
+// const xkdcData = (async () => {
+//   const response = await fetch("https://xkcd.com/info.0.json", {
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   });
+//   const data = await response.json();
+//   return data;
+// })();
+
 export default new App({
   target: document.body,
-  props: {},
+  props: {
+    title: "Language Development",
+    src: "https://imgs.xkcd.com/comics/language_development.png",
+    num: 2567,
+  },
 });
