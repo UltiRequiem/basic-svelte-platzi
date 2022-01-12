7868d4048aa7c9dd29ad227a606154aecf25dabd
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-01-12 13:35:51 -0500

    :tada: Begin

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let name;
+</script>
+
+<main>
+  <h1>Hello</h1>
+  <p />
+</main>
+
+<style>
+  :global(body) {
+    background-color: #f2eee2;
+    color: #0084f6;
+  }
+
+  :global(:root) {
+    --theme-color: purple;
+  }
+
+  p {
+    color: var(--theme-color);
+    font-size: 22px;
+  }
+
+  main {
+    text-align: center;
+  }
+</style>
